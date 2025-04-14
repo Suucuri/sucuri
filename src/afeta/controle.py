38fada238e2b182adea13e6ca796d427eff053e6
carlo-astro <carlo@ufrj.br>
2025-04-13 21:21:27 -0300

    🏺🐍 Murinus 🐁 new control _state_builder (04.13b)💋🔩

--- src/afeta/controle.py
+++ src/afeta/controle.py
@@ -18,10 +18,14 @@ Changelog
 |   **SPDX-License-Identifier:** `GNU General Public License v3.0 or later <https://is.gd/3Udt>`_.
 |   `Labase <https://labase.github.io/>`_ - `NCE <https://portal.nce.ufrj.br>`_ - `UFRJ <https://ufrj.br/>`_.
 """
+import uuid
 from collections import namedtuple
+from datetime import datetime
 from random import shuffle, sample, choice
+from typing import override
 
 DATA = namedtuple('DATA', ('text', 'tip', 'origin'))
+ME, BET = uuid.uuid4().urn[9:], [(a, b) for a in range(1, 7) for b in range(1, 5) if a >= b]
 
 
 def no_op(*_args):
@@ -31,7 +35,8 @@ def no_op(*_args):
 class Step:
     def __init__(self):
         self._nome = self._actor = self._action = self._text = self._tag = None
-        self._tip = []
+        self._tip = self._states = []
+        self._state = self._actor = {}
 
     def start(self):
         pass
@@ -70,19 +75,30 @@ class Control:
     """
 
     def __init__(self, gui, capacidade=2):
-        self._current_step = self.gui = gui
-        # self._current = namedtuple("Current", "step, handler")(Step(), no_op)
-        self._chosen = self._round = self._fotos = self._emo = None
+        # self._chosen = self._fotos = self._emo = None
         self._todas = list(range(48 * 2))
-        self.capacidade, self.gui = capacidade, gui
+        shuffle(self._todas)
+        self._me, self._bet = ME, BET
+        self._current_step = self._current_state = self.gui = gui
+        self._sentir = "Amor Raiva Tristeza Alegria Surpresa Medo".split()
+        self._emo = sample(self._sentir, 4)
+        self._chosen = [(0, choice(EMO[cs])) if cs in self._emo else (1, cs) for cs in self._sentir]
+        _, _emo = self._emo.remove(cx := choice(self._emo)), cx
+        self._foto_emo_bet = choice([0, 1]), _emo, -1
+        self._fotos = []
+        print("ME self._foto_emo_bet", ME, self._foto_emo_bet)
+
+        # self._current = namedtuple("Current", "step, handler")(Step(), no_op)
+        self._timed_todas_fotos = (datetime.now(), self._todas, self._chosen)
+        self.capacidade, self.gui, self._round = capacidade, gui, 0
         gui.subscribe(self.__class__.__name__, "play", self.play)
         gui.subscribe(self.__class__.__name__, "handle_event", self.handle_event)
-        self._sentir = "Amor Raiva Tristeza Alegria Surpresa Medo".split()
         self._msg = {}
         self.__part = namedtuple("Parts", "name, list")
         self.__step = namedtuple("Step", "recruit, round, select, choose, vote, score")
         self._part = self._parts_builder()
         self._step = self._steps_builder()
+        self._current_state = self._state_builder()
         gui.subscribe(self.__class__.__name__, "proceed_game", self.__proceed_game)
 
     def __proceed_game(self):
@@ -104,6 +120,137 @@ class Control:
         return self.__part(
             namedtuple("Part", "foto, sentir, ficha")(Foto, Sentir, Ficha),
             namedtuple("AllParts", "foto, sentir, ficha")(list(), list(), list()))
+
+    def _state_builder(self):
+        ctrl = self
+
+        class StatIter:
+            def __init__(self, seq):
+                self.seq = seq
+
+            def __iter__(self):
+                return self
+
+            def __next__(self):
+                self.seq.pop(0)
+
+        class State(Step):
+
+            def _execute(self, method, *args):
+                _ = self
+                # print("State _execute", method, *args)
+                ctrl.gui.execute(method, *args)
+
+            def _do_build(self):
+                _ = self
+                return []
+
+            def build(self, **state):
+                self._state.update(state)
+                return self._do_build()
+
+        class Inicio(State):
+            @override
+            def _do_build(self):
+                sy = self._sync__z
+                return [self.launch_modal__z, self.register__z, sy, sy, sy, self.done_modal__z]
+
+            def launch_modal__z(self, *_):
+                self._execute("open_modal", ctrl._timed_todas_fotos)
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+            def register__z(self, registry):
+                _ = registry
+                self._execute("foto_sync", ctrl._timed_todas_fotos)
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+            def _sync__z(self, timed_todas_fotos):
+                ctrl._timed_todas_fotos = min(ctrl._timed_todas_fotos, timed_todas_fotos)
+                self._fotos = [ctrl._todas.pop(0)]
+                self._execute("foto_sync", ctrl._timed_todas_fotos)
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+            def done_modal__z(self, *_):
+                ctrl._todas = fotos = ctrl._timed_todas_fotos[1]
+                ctrl._bet = BET
+                ctrl._fotos.append(fotos.pop())
+                ctrl._fotos.append(fotos.pop())
+                self._execute("done_modal", None)
+                # ctrl._part.list.foto.append(fotos.pop())
+
+        class Rodada(State):
+            @override
+            def _do_build(self):
+                # fe, em = self.foto_emo__z, self.emo_foto__z
+                return [self.start__z]
+
+            def start__z(self, *_):
+                ctrl._round += 1
+                ctrl._bet.pop(bt) if (bt := int(ctrl._foto_emo_bet[-1])) > 0 else None
+                ctrl._fotos.pop(int(ctrl._foto_emo_bet[0]))
+                ctrl._fotos.append(ctrl._todas.pop())
+                self._execute("show_fotos", ctrl._fotos)
+
+        class Selecionando(State):
+            @override
+            def _do_build(self):
+                fe, em = self.foto_emo__z, self.emo_foto__z
+                return [fe, em, fe, self.emo_foto_done__z]
+
+            def foto_emo__z(self, foto):
+                xal, _ = self._state["ops"]
+                self._execute("foto_sel", foto)
+                self._execute("emo_all")
+                self._state["foto"] = foto
+                # foto.text = data.text
+                # _foto: Parte = ctrl._part.list.foto[int(foto)]
+                self._actor[foto] = None
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+            def emo_foto__z(self, emo):
+                _, xfo = self._state["ops"]
+                self._execute(xfo, (self._state["foto"], emo))
+                self._tip.append((self._state["foto"], emo))
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+            def emo_foto_done__z(self, emo):
+                self.emo_foto__z(emo)
+                self._execute("sel_done", self._tip)
+                print("emo_foto_done__z", self._tip)
+                # self._tip.clear()
+
+                # return lambda ttf: self.launch_modal__z(ttf) if self._state["count"] else None
+
+        class Votando(State):
+            def _do_build(self):
+                cd, vt, cf = self.candidates__z, self.vote__z, self.confirma__z
+                return [cd, cd, cd, vt, cf]
+
+            def candidates__z(self, cdn):
+                xal, *_ = self._state["ops"]
+                self._tip.extend(cdn)
+                print("candidates__z", xal, self._tip, cdn)
+                self._execute(xal, self._tip)
+
+            def vote__z(self, cdn):
+                xal, mark, *_ = self._state["ops"]
+                self._execute(mark, cdn)
+                self._tip.append(cdn)
+
+            def confirma__z(self, cdn):
+                xal, mark, feito = self._state["ops"]
+                self._execute(mark, cdn)
+                self._tip.append(cdn)
+                self._execute(feito, self._tip)
+
+        def bs(sx):
+            return Selecionando().build(foto=None, ops=sx)
+        ss, sb = "emo_all emo_foto".split(), "bet_all bet_foto".split()
+        __vt = Votando().build(ops="foto_party foto_sel sync_fotos".split())
+        __states, __round, __ini = [], Rodada().build(count=3), Inicio().build(count=3)
+        # [__states.extend(seq) for seq in [__ini, __round, bs(ss), bs(ss), __vt, bs(sb), bs(sb), __vt]]
+        [__states.extend(seq) for seq in [__ini, __round, bs(ss), __vt, bs(sb), bs(sb), __vt]]
+        return StatIter(__states)
 
     def _steps_builder(self):
         ctrl = self
@@ -211,6 +358,10 @@ class Control:
         return self._todas, bet, [ft.text for ft in self._part.list.foto], self._chosen
 
     def handle_event(self, data):
+        # print("Control handle_event", self._current_step, data)
+        self._current_state.seq.pop(0)(data)
+
+    def _handle_event(self, data):
         print("Control handle_event", self._current_step, data)
         self._current_step.handle_event(data)
 
--- test/test_controle.py
+++ test/test_controle.py
@@ -1,10 +1,92 @@
 import unittest
+from datetime import datetime
 from unittest import mock
 from unittest.mock import ANY, call
 # mock.patch('vista.Body')
-from afeta.controle import Control, Step, DATA
+from afeta.controle import Control, Step  # , DATA
+
+SOON = datetime.now().now()
 
 
+class TestControl(unittest.TestCase):
+    def setUp(self):
+        hub = mock.Mock()
+        self.hub = hub
+        self.control = Control(self.hub)
+        self.cl = [call('Control', 'play', ANY), call('Control', 'handle_event', ANY),
+                   call('Control', 'proceed_game', ANY)]
+
+    def test_control_creation(self):
+        self.assertIsInstance(self.control, Control)
+        self.hub.subscribe.assert_called()
+        self.hub.subscribe.assert_has_calls(self.cl, any_order=True)
+        self.hub.subscribe.assert_any_call('Control', 'play', ANY)
+        self.assertIsInstance(a := self.control._step.recruit, Step, a)
+        self.assertEqual(self.control._part.list.foto, [])
+
+    def test_recruit_step(self):
+        self.control.handle_event(None)
+        self.hub.execute.assert_called()
+        self.hub.execute.assert_any_call('open_modal', ANY)
+
+    def test_round_step(self):
+        self._run_step(ext=(0, 0))
+        self.hub.execute.assert_any_call('show_fotos', ANY)
+
+    def test_register_step(self):
+        [self.control.handle_event(dt) for dt in [None, (datetime.now(), [1, 2, 3, 4, 5])]]
+        self.hub.execute.assert_any_call('foto_sync', ANY)
+
+    def _run_step(self, evt=None, ext=()):
+        dn = (datetime.now(), [1, 2, 3, 4, 5])
+        ev = evt or ([None, dn, dn, dn, (SOON, [1, 2, 3, 4, 5])] + list(ext))
+        [self.control.handle_event(dt) for dt in ev]
+
+    def test_sync_step(self):
+        self._run_step()
+        self.hub.execute.assert_any_call('foto_sync', ANY)
+
+    def test_don_modal_step(self):
+        dn = (datetime.now(), [1, 2, 3, 4, 5])
+        self._run_step([None, dn, dn, dn, dn, dn])
+        self.hub.execute.assert_any_call('done_modal', ANY)
+
+    def test_sel_foto_step(self):
+        self._run_step(ext=(0, 0, 0))
+        # foto_list = self.control._part.list.foto
+        foto_list = self.control._fotos
+        self.assertIn(5, foto_list)
+        self.hub.execute.assert_any_call('foto_sel', 0)
+        self.hub.execute.assert_any_call('emo_all')
+
+    def test_sel_emo_step(self):
+        self._run_step(ext=(0, 0, 0, 3))
+        self.hub.execute.assert_any_call('emo_foto', ANY)
+        # self.hub.execute.assert_any_call('emo_foto', (5, 3))
+
+    def test_sel_foto_emo_step2(self):
+        self._run_step(ext=(0, 0, 0, 3, 1))
+        self.hub.execute.assert_any_call('foto_sel', 1)
+        self.control.handle_event(2)
+        self.hub.execute.assert_any_call('emo_foto', (1, 2))
+        self.hub.execute.assert_any_call('sel_done', [(0, 3), (1, 2)])
+        # self.hub.execute.assert_any_call('sel_done', ANY)
+        # self.assertEqual(self.control._tip, [])
+
+    def test_voting_step(self):
+        self._run_step(ext=(0, 0, 0, 3, 1, 2, [(1, 1), (0, 0)]))
+        self.hub.execute.assert_any_call('foto_party', ANY)
+        self.control.handle_event([(0, 3), (1, 2)])
+        self.hub.execute.assert_any_call('foto_party', [(1, 1), (0, 0), (0, 3), (1, 2)])
+        self.control.handle_event([(1, 0), (2, 1)])
+        self.hub.execute.assert_any_call('foto_party', [(1, 1), (0, 0), (0, 3), (1, 2), (1, 0), (2, 1)])
+        self.control.handle_event([(1, 1)])
+        self.hub.execute.assert_any_call('foto_sel', ANY)
+        # self.control.handle_event(2)
+        # self.hub.execute.assert_any_call('emo_foto', (1, 2))
+
+
+"""
 class TestControl(unittest.TestCase):
     def setUp(self):
         hub = mock.Mock()
@@ -63,7 +145,7 @@ class TestControl(unittest.TestCase):
         self.assertEqual("Select", self.control._current_step.__class__.__name__, )
         # self.hub.execute.assert_called_once_with("view_update")
         self.hub.execute.assert_has_calls([call("view_update"), call("view_update")], any_order=True)
-
+"""
 
 if __name__ == '__main__':
     unittest.main()

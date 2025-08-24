576d6fd2f9cc691f4168cc684e908a0d9784e0cb
carlo-astro <carlo@ufrj.br>
2025-08-24 01:09:45 -0300

    🚧⚝Golden Penta⭐ ✨T.O.  Head (08.24a)🕸️🏺⛲⭐🟥

--- src/alite/html_build.py
+++ src/alite/html_build.py
@@ -15,6 +15,7 @@ Changelog
 .. versionadded::    25.08
    |br| Initial builder implementation (21).
    |br| added list comprehension (23).
+   |br| added read and post to head (25).
 
 |   **Open Source Notification:** This file is part of open source program **Alite**
 |   **Copyright © 2025  Carlo Oliveira** <carlo@nce.ufrj.br>,
@@ -23,6 +24,7 @@ from collections import namedtuple
 from browser import document as doc, html as ht
 
 NO_DICT = {}
+NO_E = "<-- {} -->"
 
 
 class TemplateBuilder:
@@ -46,15 +48,21 @@ class TemplateBuilder:
     >>> elements = builder.build(template_data)
     """
 
-    HTML = namedtuple('HTML', "n d h a z s p x r u l y i k m")(
+    @staticmethod
+    def head(*kwargs):
+        # print("head(*kwargs)", kwargs)
+        return [doc.head <= ents for ents in kwargs]
+
+    HTML = namedtuple('HTML', "n d h a z s p x r u l y i k m H B")(
         n=ht.NAV, d=ht.DIV, h=ht.H1, a=ht.A, z=ht.SECTION, s=ht.SPAN, p=ht.P, x=ht.H2, r=ht.HR, u=ht.UL,
-        l=ht.LI, y=ht.H4, i=ht.IMG, k=ht.LINK, m=ht.META, )
+        l=ht.LI, y=ht.H4, i=ht.IMG, k=ht.LINK, m=ht.META, H=head, B=ht.BODY)
 
     def __init__(self):
-        """Initialize the TemplateBuilder with HTML element references and an empty names dictionary."""
+        """Initialize the TemplateBuilder with HTML element references and an empty names, macros dictionary."""
         super().__init__()  # noqa
         self.t = TemplateBuilder.HTML
         self.names = {}
+        self.macros = {}
 
     def parse_template(self, data):
         """Parse template data and generate corresponding HTML elements.
@@ -80,18 +88,29 @@ class TemplateBuilder:
         """
 
         def do_parse(tg, **kw):
+            if (not isinstance(tg, str)) or (len(tg) < 3):
+                return [NO_E.format(tg + "begin")]
             match tg[0]:
-                case 'h':
-                    coded = tags[tg[1]](**kw)
-                    self.names[tg] = coded
+                case 'h' | 'd':
+                    if tg[1] not in tags:
+                        return [kw]
+                    elif tg[1] in "HB":
+                        _parse = [_tg for tag, arg in kw.items() if tag[2] not in "_" for _tg in do_parse(tag, **arg)]
+                        _tags = tags[tg[1]](*_parse)
+                        return [_tags]
+                    coded = tags[tg[1]](**kw) if tg[1] in tags else NO_E.format(tg + str(kw))
+                    self.macros[tg] = coded if coded else NO_E.format(tg + str(kw))
                     return [coded]
                 case 'l':
+                    # print("case 'l'", tg, kw, "case 'list'", self.list_comprehension(**kw))
                     return self.list_comprehension(**kw)
                 case _:
-                    return [self.t.d()]
+                    return [NO_E.format(tg + "end")]
 
         tags = self.t._asdict()
-        code = [entity for tag, kw in data.items() for entity in do_parse(tag, **kw)]
+        # print("data", data)
+        code = [entity for tag, kw in data.items() for entity in do_parse(tag, **kw if isinstance(kw, dict) else data)]
+        # print("code", code)
         return code
 
     def get_names(self, td):
@@ -127,15 +146,16 @@ class TemplateBuilder:
         list
             A list of generated HTML elements.
         """
-        nm = {l_x: 0} if not isinstance(n := self.get_names(l_x), dict) else n
+        nm = dict(self.names.get(l_x, []))
+        # nm = {l_x: 0} if not isinstance(n := self.get_names(l_x), type) else n
 
         def do_expression(data):
-            nm.update(data)
+            nm.update(**data)
             nmc = {l_x: nm}
             # print("do_expression", nm, "nmc", nmc, "data", data)
-            return self.parse_template(nmc)[0]
+            return self.parse_template(nmc)[-1]
 
-        # print("list_comprehension", l_x, "data", l_y, self.get_names(l_x))
+        # print("list_comprehension", l_x, "data", l_y, type(self.get_names(l_x)), "nm", nm)
         return [do_expression(data) for data in l_y]
 
     def build(self, data: dict = None):
@@ -159,13 +179,14 @@ class TemplateBuilder:
 
         def register_names(fix, dicionario):
             for k, v in dicionario.items():
-                if k[0] == "h" and k[-1] in "0123456789_":
+                if k[0] in "hd" and k[-1] in "0123456789_":
                     self.names[f"{k}_{fix}_"] = v
-                register_names(f"{k}_{fix}_", v) if isinstance(v, dict) else None
+                register_names(f"{k}", v) if isinstance(v, dict) else None
+
+        # print("build_data", data)
 
         register_names("", data) if data is not None else None
         data = self.parse_template(data or {})
-        _ = [doc.head <= tg for tg in data]
         doc.title = "LABASE"
         return data
 
@@ -184,7 +205,8 @@ def _populate_html():
     libs = "bulma/1.0.4/css/bulma.min.css font-awesome/7.0.0/css/all.min.css /css/labase.css".split()
     dt = [dict(href=lb) for lb in libs]
     di = dict(rel="shortcut icon", href="/_media/suucurijuba.png", type="image/x-icon")
-    tg = dict(hk0=dict(rel="stylesheet"), lk0=dict(l_x="hk0__", l_y=dt), hk1=di)
+    tg = dict(hk_0=dict(rel="stylesheet"), lk0=dict(l_x="hk_0_dH0_", l_y=dt), hk1=di)
+    tg = dict(dH0=tg)
     return tg
 
 
--- test/test_html_build.py
+++ test/test_html_build.py
@@ -30,14 +30,28 @@ class MyBrock:
     def __init__(self, data):
         self.data = data
         self.k = self.link
+        self.d = self.link
+        self.Z = self.link
+        self.H = self.head
+        self.kwarg = {}
+
+    def head(self, *args, **kwargs):
+        self.kwarg = kwargs
+        self.data.append([self.H, args, kwargs])
+        return self.H
 
     def link(self, *args, **kwargs):
+        self.kwarg = kwargs
         self.data.append([self.k, args, kwargs])
         return self.k
 
+    def _repr__(self):
+        return f'<MyBrock args={self.kwarg}>'
+
     # noinspection SpellCheckingInspection
     def _asdict(self):
-        return dict(k=self.link)
+        e = self.link
+        return dict(k=e, d=e, Z=e, H=self.head)
 
 
 class TestHtmlBuild(unittest.TestCase):
@@ -46,21 +60,23 @@ class TestHtmlBuild(unittest.TestCase):
         self.dt = []
         self.tb.t = MyBrock(self.dt)
 
+    def _do_mock(self, mock_ht):
+        mock_ht.return_value = MyBrock({})
+        return self.tb.t
+
     def test_tb(self):
         self.assertIsInstance(self.tb, Teb)
 
     @unittest.mock.patch(HTML)
     def test_html(self, mock_ht):
-        mock_ht.return_value = MyBrock({})
-        ti = self.tb.t
+        ti = self._do_mock(mock_ht)
         self.html = self.tb.build(dict(hk0=dict(rel="stylesheet")))
         self.assertEqual(ti.k, ti.link)
         self.assertIn(ti.k, self.html)
 
     @unittest.mock.patch(HTML)
     def test_head_html(self, mock_ht):
-        mock_ht.return_value = MyBrock({})
-        ti = self.tb.t
+        ti = self._do_mock(mock_ht)
         tgs = {f"hk{ix}": dict(rel="stylesheet", href=f"tg{ix}") for ix in range(2)}
         self.html = self.tb.build(tgs)
         self.assertIn(ti.k, self.html)
@@ -71,16 +87,27 @@ class TestHtmlBuild(unittest.TestCase):
 
     @unittest.mock.patch(HTML)
     def test_html_comprehension(self, mock_ht):
-        mock_ht.return_value = MyBrock({})
-        ti = self.tb.t
+        ti = self._do_mock(mock_ht)
         dt = [dict(href="tg0"), dict(href="tg1")]
-        tg = dict(hk0=dict(rel="stylesheet"), lk0=dict(l_x="hk0__", l_y=dt))
+        tg = dict(hk_0=dict(rel="stylesheet"), lk0=dict(l_x="hk_0__", l_y=dt))
         self.html = self.tb.build(tg)
         self.assertIn(ti.k, self.html)
-        self.assertEqual(3, len(self.tb.t.data))
+        self.assertEqual(3, len(self.tb.t.data), self.tb.t.data)
         self.assertEqual(3, len(self.html), self.html)
         self.assertEqual(ti.k, self.html[0])
         self.assertEqual(3, sum(1 for _ in self.html if ti.k == _), self.html)
+
+    @unittest.mock.patch(HTML)
+    def test_html_with_head(self, mock_ht):
+        ti = self._do_mock(mock_ht)
+        tgs = {"dH0": {f"hk{ix}": dict(rel="stylesheet", href=f"tg{ix}") for ix in range(2)}}
+        # print("test_html_with_head", tgs)
+        self.html = self.tb.build(tgs)
+        self.assertIn(ti.H, self.html)
+        self.assertIn("hk0_dH0_", self.tb.names)
+        self.assertEqual(3, len(self.dt), self.dt)
+        self.assertEqual(1, len(self.html), self.html)
+        self.assertIn(ti.H, self.html)
 
 
 if __name__ == '__main__':
